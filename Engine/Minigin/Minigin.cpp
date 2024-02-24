560c9ddc73479700524c1150513710cb0ec52480
VryensSebastiaan <119673781+SebaTheProgrammer@users.noreply.github.com>
2024-02-24 09:39:36 +0100

    Start added
    The base for this engine

---
+++ Engine/Minigin/Minigin.cpp
@@ -0,0 +1,117 @@
+#include <stdexcept>
+#define WIN32_LEAN_AND_MEAN 
+#include <windows.h>
+#include <SDL.h>
+#include <SDL_image.h>
+#include <SDL_ttf.h>
+#include "Minigin.h"
+#include "InputManager.h"
+#include "SceneManager.h"
+#include "Renderer.h"
+#include "ResourceManager.h"
+#include <iostream>
+#include <chrono>
+
+SDL_Window* g_window{};
+
+void PrintSDLVersion()
+{
+	SDL_version version{};
+	SDL_VERSION(&version);
+	printf("We compiled against SDL version %u.%u.%u ...\n",
+		version.major, version.minor, version.patch);
+
+	SDL_GetVersion(&version);
+	printf("We are linking against SDL version %u.%u.%u.\n",
+		version.major, version.minor, version.patch);
+
+	SDL_IMAGE_VERSION(&version);
+	printf("We compiled against SDL_image version %u.%u.%u ...\n",
+		version.major, version.minor, version.patch);
+
+	version = *IMG_Linked_Version();
+	printf("We are linking against SDL_image version %u.%u.%u.\n",
+		version.major, version.minor, version.patch);
+
+	SDL_TTF_VERSION(&version)
+	printf("We compiled against SDL_ttf version %u.%u.%u ...\n",
+		version.major, version.minor, version.patch);
+
+	version = *TTF_Linked_Version();
+	printf("We are linking against SDL_ttf version %u.%u.%u.\n",
+		version.major, version.minor, version.patch);
+}
+
+dae::Minigin::Minigin(const std::string &dataPath)
+{
+	PrintSDLVersion();
+	
+	if (SDL_Init(SDL_INIT_VIDEO) != 0) 
+	{
+		throw std::runtime_error(std::string("SDL_Init Error: ") + SDL_GetError());
+	}
+
+	g_window = SDL_CreateWindow(
+		"Programming 4 assignment",
+		SDL_WINDOWPOS_CENTERED,
+		SDL_WINDOWPOS_CENTERED,
+		640,
+		480,
+		SDL_WINDOW_OPENGL
+	);
+	if (g_window == nullptr) 
+	{
+		throw std::runtime_error(std::string("SDL_CreateWindow Error: ") + SDL_GetError());
+	}
+
+	Renderer::GetInstance().Init(g_window);
+
+	ResourceManager::GetInstance().Init(dataPath);
+}
+
+dae::Minigin::~Minigin()
+{
+	Renderer::GetInstance().Destroy();
+	SDL_DestroyWindow(g_window);
+	g_window = nullptr;
+	SDL_Quit();
+}
+
+void dae::Minigin::Run(const std::function<void()>& load)
+{
+	load();
+
+	auto& renderer = Renderer::GetInstance();
+	auto& sceneManager = SceneManager::GetInstance();
+	auto& input = InputManager::GetInstance();
+
+	// todo: this update loop could use some work.
+	bool loop = true;
+	auto last_time = std::chrono::high_resolution_clock::now();
+	float lag = 0.0f;
+	float fixed_time_step = 16.f;
+
+	while (loop)
+	{
+		loop = input.ProcessInput();
+
+		const auto current_time = std::chrono::high_resolution_clock::now();
+		std::chrono::duration<float> elapsed_time = current_time - last_time;
+		const float delta_time = elapsed_time.count();
+
+		last_time = current_time;
+		lag += delta_time;
+
+		while ( lag >= fixed_time_step )
+		{
+			sceneManager.FixedUpdate( fixed_time_step );
+			lag -= fixed_time_step;
+		}
+
+		sceneManager.Update();
+		renderer.Render();
+
+		//const auto sleep_time = current_time + std::chrono::milliseconds( fixed_time_step ) - std::chrono::high_resolution_clock::now();
+		//this_thread::sleep_for( sleep_time );
+	}
+}
