dafbe94f11d95e2292aa3f37cfe6c0904634bf43
VryensSebastiaan <119673781+SebaTheProgrammer@users.noreply.github.com>
2024-03-10 11:32:18 +0100

    imGui starter window implemented

--- Engine/Minigin/InputManager.cpp
+++ Engine/Minigin/InputManager.cpp
@@ -1,13 +1,20 @@
 #include <SDL.h>
 #include "InputManager.h"
+#include "../3rdParty/imgui_impl_sdl2.h"
 
 bool dae::InputManager::ProcessInput()
 {
+	//ZeroMemory( &m_CurrentState, sizeof( XINPUT_STATE ) );
+	//XInputGetState( 0, &m_CurrentState );
+
 	SDL_Event e;
 	while (SDL_PollEvent(&e)) {
 		if (e.type == SDL_QUIT) {
 			return false;
 		}
+		//proces event for IMGUI
+		ImGui_ImplSDL2_ProcessEvent(&e);
+
 		if (e.type == SDL_KEYDOWN) {
 			
 		}
