6d57a28e5ef44978b9062700b7dcbb50ae08adb0
VryensSebastiaan <119673781+SebaTheProgrammer@users.noreply.github.com>
2024-03-20 20:49:48 +0100

    Update GameEvents.h
    Eventtype and event

--- Engine/Minigin/GameEvents.h
+++ Engine/Minigin/GameEvents.h
@@ -4,6 +4,22 @@ namespace dae
 {
 	class GameEvents
 	{
+		enum EventType {
+			LEVEL_STARTED,
+			PLAYER_SPAWNED,
+			PLAYER_DIED,
+			PLAYER_HIT,
+			PLAYER_WON,
+			PLAYER_LOST,
+			LEVEL_ENDED,
+		};
+
+		struct Event {
+			static const uint8_t MAX_ARGS = 8;
+
+			EventType m_type;
+			uint8_t m_numArgs;
+		};
 
 	};
 
