c08565dd02c8678387e75fcb77cf13dd5ab1699b
VryensSebastiaan <119673781+SebaTheProgrammer@users.noreply.github.com>
2024-06-06 11:11:08 +0200

    fixed coily multiplayer

--- Engine/QBert/Coily.cpp
+++ Engine/QBert/Coily.cpp
@@ -238,7 +238,7 @@ void Coily::FollowPlayer( int playerRow1, int playerCol1, int playerRow2, int pl
 		int colDifference2 = std::abs( m_Col - playerCol2 );
 
 		// Compare the sum of row and column differences to decide the target
-		if ( rowDifference1 + colDifference1 >= rowDifference2 + colDifference2 ) {
+		if ( rowDifference1 + colDifference1 <= rowDifference2 + colDifference2 ) {
 			targetRow = playerRow1;
 			targetCol = playerCol1;
 		}
